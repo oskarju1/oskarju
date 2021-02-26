d188a0fc79b8aa676b87c1ef1904f7d2234593f9
Oskar <wesoly.oski@interia.pl>
2021-02-26 17:38:13 +0100

    byle co

---
+++ src/com/company/Animal.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.File;
+
+public class Animal {
+    String species;
+    Double weight;
+    String name;
+    File pic;
+
+    void feed(){
+        weight += 1;
+        System.out.println("Thx for food");
+    }
+}
